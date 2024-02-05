235269f8568acb515f703482a710de0f434f9fc6
Gavin Franklin <60778485+gmitchellf@users.noreply.github.com>
2024-02-05 15:58:45 +0000

    JSON Schema's Added & HTML Compliancy

--- js/scripts.js
+++ js/scripts.js
@@ -14,24 +14,3 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
-
-document.addEventListener('DOMContentLoaded', function () {
-    const sections = document.querySelectorAll('section');
-    const navLinks = document.querySelectorAll('nav a');
-
-    function makeLinkActive() {
-        sections.forEach((section, index) => {
-            const rect = section.getBoundingClientRect();
-            if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-                navLinks.forEach((link) => {
-                    link.classList.remove('active');
-                });
-                navLinks[index].classList.add('active');
-            }
-        });
-    }
-
-    makeLinkActive();
-
-    window.addEventListener('scroll', makeLinkActive);
-});
